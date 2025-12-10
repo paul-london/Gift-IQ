5bd766518f2ba879deb15646690785ba318db5cc
Iyad <iyadshobaki82@gmail.com>
2025-12-10 18:07:11 -0500

    chore: add more general folders and files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="app">Smart Gift Planner</div>
-    </>
+    <div className="page">
+      <div className="page__content">Smart Gift Planner</div>
+    </div>
   );
 }
 
