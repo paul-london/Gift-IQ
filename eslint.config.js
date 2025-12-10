4d7bd2dd81b99da968b6d127b8dc11b2488519be
Iyad <iyadshobaki82@gmail.com>
2025-12-10 17:36:34 -0500

    chore: clean default code, add config changes and organize file structure

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      // Disable prop-types validation.
+      "react/prop-types": 0,
     },
+    // Add an overrides array. Without this, ESLint could only be run via
+    // the npm run lint command. Adding this will allow it to be run also
+    // via the global `npx eslint .` command.
+    overrides: [
+      {
+        files: ["*.js", "*.jsx"],
+      },
+    ],
   },
-])
+]);
---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app">Smart Gift Planner</div>
+    </>
+  );
+}
+
+export default App;
