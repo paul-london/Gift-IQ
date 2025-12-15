7be683403cda7f3b57068d14e930a9022718250b
Iyad <iyadshobaki82@gmail.com>
2025-12-14 23:51:03 -0500

    fix: add back base in order for the deploy to be triggered

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/smart_gift_planner",
   server: {
     port: 3000,
   },
