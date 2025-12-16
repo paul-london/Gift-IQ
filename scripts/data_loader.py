d67d6de7363f700d34f3f339d5876a2fda088fac
Paul London <palondon@hotmail.com>
2025-12-16 10:42:48 -0500

    Update data_loader.py

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,11 +1,15 @@
-# data_loader.py
 from datasets import load_dataset
 
 def stream_products():
-    """
+        """
     Stream products dataset from Hugging Face.
     Returns a generator of product dicts.
     """
-    dataset = load_dataset("json", data_files="https://huggingface.co/datasets/palondon/amazonproducts/resolve/main/products_full_clean_categories.json", streaming=True)
-    for item in dataset:
+    dataset = load_dataset(
+        "json",
+        data_files="https://huggingface.co/datasets/palondon/amazonproducts/resolve/main/products_full_clean_categories.json",
+        streaming=True,
+    )
+
+    for item in dataset["train"]:
         yield item
